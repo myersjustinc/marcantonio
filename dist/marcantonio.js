368038c647b3a218cd2ac58fa053097f93c0cbde
Justin Myers <JMyers@newshour.org>
2012-11-13 17:58:29 -0500

    Added base functionality

--- dist/marcantonio.js
+++ dist/marcantonio.js
@@ -2,3 +2,126 @@
 * https://github.com/myersjustinc/marcantonio
 * Copyright (c) 2012 Justin Myers; Licensed BSD */
 
+(function(window) {
+    "use strict";
+    
+    var $ = window.jQuery;
+    
+    var Marc = window.Marc || {};
+    window.Marc = Marc;
+    
+    Marc.R = window.Raphael;
+    
+    Marc.Drawing = function Drawing(container) {
+        var contElem;
+        if (typeof container === 'string') {
+            // container is an element ID
+            contElem = window.document.getElementById(container);
+        } else if (typeof container.append === 'function') {
+            // container is a jQuery object
+            contElem = container[0];
+        } else if (typeof container.nodeName === 'string') {
+            // container is a DOM node
+            contElem = container;
+        } else {
+            throw new Error("Invalid argument for Drawing container");
+        }
+        
+        this.paper = Marc.R(contElem);
+        
+        var views = {};
+        this.addView = function addView(viewName, pathData) {
+            if (views[viewName]) {
+                views[viewName].destroy();
+            }
+            views[viewName] = new Marc.View(this, pathData);
+            return views[viewName];
+        };
+        this.getView = function getView(viewName) {
+            return views[viewName];
+        };
+        
+        this.hideAllViews = function hideAllViews() {
+            for (var viewName in views) {
+                if (views.hasOwnProperty(viewName)) {
+                    views[viewName].hide();
+                }
+            }
+        };
+        this.showView = function showView(viewName) {
+            if (!views[viewName]) {return;}
+            this.hideAllViews();
+            views[viewName].show();
+        };
+    };
+    
+    Marc.View = function View(drawing, pathData) {
+        pathData = pathData || {};
+        drawing.hideAllViews();
+        
+        this.areas = {};
+        for (var pathName in pathData) {
+            if (pathData.hasOwnProperty(pathName)) {
+                this.areas[pathName] = new Marc.Area(drawing, pathName,
+                    pathData[pathName]);
+            }
+        }
+        
+        this.hide = function hide() {
+            for (var areaName in this.areas) {
+                if (this.areas.hasOwnProperty(areaName)) {
+                    this.areas[areaName].element.hide();
+                }
+            }
+        };
+        this.show = function show() {
+            for (var areaName in this.areas) {
+                if (this.areas.hasOwnProperty(areaName)) {
+                    this.areas[areaName].element.show();
+                }
+            }
+        };
+        this.destroy = function destroy() {
+            for (var areaName in this.areas) {
+                if (this.areas.hasOwnProperty(areaName)) {
+                    this.areas[areaName].element.remove();
+                }
+            }
+        };
+        
+        this.setColors = function setColors(formatter) {
+            for (var areaName in this.areas) {
+                if (this.areas.hasOwnProperty(areaName)) {
+                    var area = this.areas[areaName];
+                    area.setFill(formatter(area));
+                }
+            }
+        };
+        this.setStrokes = function setStrokes(formatter) {
+            for (var areaName in this.areas) {
+                if (this.areas.hasOwnProperty(areaName)) {
+                    var area = this.areas[areaName];
+                    area.setStroke(formatter(area));
+                }
+            }
+        };
+    };
+    
+    Marc.Area = function Area(drawing, areaName, pathData) {
+        this.element = drawing.paper.path(pathData);
+        this.name = areaName;
+        
+        this.getFill = function getFill() {
+            return this.element.attr('fill');
+        };
+        this.setFill = function setFill(newFill) {
+            this.element.attr('fill', newFill);
+        };
+        this.getStroke = function getStroke() {
+            return this.element.attr('stroke');
+        };
+        this.setStroke = function setStroke(newStroke) {
+            this.element.attr('stroke', newStroke);
+        };
+    };
+}(this));
--- test/marcantonio-test.js
+++ test/marcantonio-test.js
@@ -1,7 +1,8 @@
 /*global QUnit:false, module:false, test:false, asyncTest:false, expect:false*/
 /*global start:false, stop:false ok:false, equal:false, notEqual:false, deepEqual:false*/
 /*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, raises:false*/
-(function($) {
+/*global Marc:false*/
+(function($, Marc) {
     /*
         ======== A Handy Little QUnit Reference ========
         http://docs.jquery.com/QUnit
@@ -20,4 +21,49 @@
             notStrictEqual(actual, expected, [message])
             raises(block, [expected], [message])
     */
-}(jQuery));
+    
+    var boxPaths = {
+        "A": "M 50 50 L 100 50 L 100 100 L 50 100 z",
+        "B": "M 100 50 L 150 50 L 150 100 L 100 100 z",
+        "C": "M 50 100 L 100 100 L 100 150 L 50 150 z",
+        "D": "M 100 100 L 150 100 L 150 150 L 100 150 z"
+    };
+    
+    test("Basic functionality", function() {
+        $('<div id="boxes"></div>').appendTo('#qunit-fixture');
+        var drawing = new Marc.Drawing('boxes');
+        var view = drawing.addView('boxes', boxPaths);
+        var areaA = view.areas['A'];
+        
+        ok(drawing, 'Drawing object exists');
+        ok(view, 'View object exists');
+        ok(areaA, 'Area object exists');
+        deepEqual(areaA.element.paper, drawing.paper, 'Area exists on drawing');
+    });
+    
+    test("Coloring paths", function() {
+        $('<div id="boxes"></div>').appendTo('#qunit-fixture');
+        var view = new Marc.Drawing('boxes').addView('boxes', boxPaths);
+        var areaA = view.areas['A'];
+        
+        view.setColors(function(area) {
+            if (area.name === 'A') {
+                return '#009';
+            } else if (area.name === 'B') {
+                return '#900';
+            } else if (area.name === 'C') {
+                return '#090';
+            } else {
+                return '#909';
+            }
+        });
+        view.setStrokes(function(area) {
+            return '#ccc';
+        });
+        
+        deepEqual(Marc.R.getRGB(areaA.getFill()), Marc.R.getRGB('#009'),
+            'Area fill set properly');
+        deepEqual(Marc.R.getRGB(areaA.getStroke()), Marc.R.getRGB('#ccc'),
+            'Area stroke set properly');
+    });
+}(jQuery, Marc));
